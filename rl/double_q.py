08e4faebf179b7814bbc31db566c63605817e3c4
lukas <113726590+BasedLukas@noreply.github.com>
2024-03-16 11:52:23 +0100

    refactor

--- rl/double_q.py
+++ rl/double_q.py
@@ -1,121 +1,175 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from typing import Dict, Optional, Tuple
 
 
-def policy(state: str, epoch: int, q1:dict, q2={})-> int:
+def argmax_a(state: str, q: Dict) -> int:
     """
-    Epsilon-greedy policy based on Q-values.
-    Q2: Optional 2nd Q dict for double Q learning
-    Returns action:int
+    Find the action that maximizes the Q-value in a given state.
+
+    Args:
+    - state: The current state.
+    - q: The Q-value dictionary.
+
+    Returns:
+    - An integer representing the action that maximizes the Q-value.
     """
-    # default value for q2, to prevent interference when not used
-    # when used we want default to be 0
-    default = -float('inf') if q2 == {} else 0
-    # epsilon decay over time
-    eps = epsilon * (eps_decay ** epoch)
-    
-    # number of possible actions for the current state
-    number_of_possible_actions = len(transitions[state])
-    
-    # exploration
-    if np.random.random() < eps:
-        print("Epsilon event, exploring...")
-        return np.random.choice([i for i in range(number_of_possible_actions)])
-
-    # get values for all possible actions in current state
-    action_values1 = [q1.get((state, action), 0) for action in range(number_of_possible_actions)]
-    action_values2 = [q2.get((state, action), default) for action in range(number_of_possible_actions)]
-
-    # there may be multiple best actions
-    max_value = max(action_values1 + action_values2)
-    best_actions = [
-        action for action, (v1, v2) in enumerate(zip(action_values1, action_values2))
-        if v1 == max_value or v2 == max_value
-        ]
-    
-    # sorandomly select from the best scoring actions
+    if state == 'terminal':
+        return 0
+    actions = range(len(transitions[state]))
+    values = [q.get((state, action), 0) for action in actions]
+    max_value = max(values)
+    best_actions = [action for action, value in enumerate(values) if value == max_value]
     return np.random.choice(best_actions)
-    
 
-def get_reward(state:str)-> float:
+
+def max_a(state: str, q: Dict) -> float:
     """
-    returns reward for transitioning into a state
-    state: the state you just transitioned into.
+    Find the maximum Q-value for any action in a given state.
+
+    Args:
+    - state: The current state.
+    - q: The Q-value dictionary.
+
+    Returns:
+    - The maximum Q-value for the given state.
+    """
+    if state == 'terminal':
+        return 0 
+    return max([q.get((state, action), 0) for action in range(len(transitions[state]))])
+
+
+def policy(
+        state: str, 
+        epoch: int, 
+        q1: Dict, 
+        q2: Optional[Dict] = None
+        ) -> int:
+    """
+    Select an action based on the epsilon-greedy policy derived from Q-values.
+
+    Args:
+    - state: The current state in the environment.
+    - epoch: The current epoch of training, used to decay epsilon.
+    - q1: The primary Q-value dictionary.
+    - q2: An optional secondary Q-value dictionary for double Q-learning.
+
+    Returns:
+    - An integer representing the chosen action.
+    """
+
+    eps = epsilon * (eps_decay ** epoch)
+    number_of_possible_actions = len(transitions[state])
+
+    #exploration
+    if np.random.random() < eps:
+        return np.random.choice(range(number_of_possible_actions))
+
+    action1 = argmax_a(state, q1)
+    if not q2:
+        return action1
+    
+    action2 = argmax_a(state, q2)
+    return np.random.choice([action1, action2])
+
+
+def get_reward(state: str) -> float:
+    """
+    Returns the reward for transitioning into a given state.
+
+    Args:
+    - state: The state transitioned into.
+
+    Returns:
+    - A float representing the reward for that transition.
+
+    Raises:
+    - ValueError: If an invalid state is provided.
     """
     if state == "a":
-        raise ValueError("a should not be passsed as a param as it's the starting state")
-    if state == 'b':
-        return 0
-    if state == 'terminal':
+        raise ValueError("a should not be passed as a param as it's the starting state")
+    if state == 'b' or state == 'terminal':
         return 0
     if 'c' in state:
         return np.random.normal(-0.1, 1)
     raise ValueError(f"state: {state} not recognized")
 
 
-def max_aq(state: str, q:dict)-> float:
+def q_update(
+        state: str, 
+        action: int, 
+        new_state: str, 
+        reward: float, 
+        alpha: float, 
+        q: Dict
+    ) -> None:
     """
-    max a for Q(s,a)
-    returns max value for a state if greedy action is taken
+    In-place update of Q-values for Q-learning.
+
+    Args:
+        state: The current state.
+        action: The action taken in the current state.
+        new_state: The state reached after taking the action.
+        reward: The reward received after taking the action.
+        alpha: The learning rate.
+        q: The Q-values dictionary.
     """
-    if state == 'terminal':
-        return 0 
-    q_values = [q.get((state, action), 0) for action in range(len(transitions[state]))]
-    return max(q_values)
-
-
-def argmax_a(state:str, q:dict):
-    if state == 'terminal':
-        return 0
-    n_of_actions_for_state = len(transitions[state])
-    values = [q.get((state, action), 0) for action in range(n_of_actions_for_state)]
-    indices = [i for i, x in enumerate(values) if x == max(values)]
-    return np.random.choice(indices)
-
-
-def td_update(state, action, new_state, reward, alpha, q):
-    """
-    In place update of Q.
-    
-    :param state: The current state.
-    :param action: The action taken in the current state.
-    :param new_state: The state reached after taking the action.
-    :param reward: The reward received after taking the action.
-    :param alpha: The learning rate.
-    :param q: q values dict
-
-    """
-    # Current Q-value estimation
-    current_q = q.get((state, action), 0)
-    # Maximum Q-value for the next state
-    max_next = max_aq(new_state, q)
-    # TD Target
-    target = reward + gamma * max_next
-    # TD Error
-    td_error = target - current_q
-    # TD Update
-    update = alpha * td_error
+    current_q = q.get((state, action), 0)  # Current Q-value estimation
+    max_next = max_a(new_state, q)  # Maximum Q-value for the next state
+    target = reward + gamma * max_next  # TD Target
+    td_error = target - current_q  # TD Error
+    update = alpha * td_error  # TD Update
     q[(state, action)] = current_q + update
 
 
-def double_q_update(state, action, new_state, reward, alpha, q1, q2):
-    # randomly select one q to be updated
-    qs = [q1,q2]
-    random.shuffle(qs)
-    qa, qb = qs[0], qs[1]
+def double_q_update(
+        state: str, 
+        action: int, 
+        new_state: str, 
+        reward: float, 
+        alpha: float, 
+        q1: Dict, 
+        q2: Dict
+    ) -> None:
+    """
+    In-place update of Q-values for Double Q-learning.
 
-    current_q = qa.get((state, action), 0)
-    best_action = argmax_a(new_state, qa)
-    target = reward + gamma * qb.get((new_state, best_action), 0)
-    error = target - current_q
-    update = alpha * error
-    qa[(state, action)] = update + current_q
+    Args:
+        state: The current state.
+        action: The action taken in the current state.
+        new_state: The state reached after taking the action.
+        reward: The reward received after taking the action.
+        alpha: The learning rate.
+        q1: The first Q-values dictionary.
+        q2: The second Q-values dictionary.
+    """
+    qs = [q1, q2]  # List of Q dictionaries
+    random.shuffle(qs)  # Randomly shuffle to choose one for updating
+    qa, qb = qs  # qa is the Q to update, qb is used for target calculation
+
+    current_q = qa.get((state, action), 0)  # Current Q-value estimation
+    best_action = argmax_a(new_state, qa)  # Best action based on qa
+    target = reward + gamma * qb.get((new_state, best_action), 0)  # TD Target using qb
+    error = target - current_q  # TD Error
+    update = alpha * error  # TD Update
+    qa[(state, action)] = current_q + update
 
 
+def simulate(
+        epoch: int, 
+        q: Dict, 
+        q2: Optional[Dict] = None
+    ) -> None:
+    """
+    Simulate an epoch of the agent's interaction with the environment, updating Q-values based on observed transitions.
 
-def simulate(epoch:int, q:dict, q2=None):
-    double = isinstance(q2, dict)
+    Args:
+        epoch: The current epoch of the simulation.
+        q: The Q-values dictionary for Q-learning or the primary Q-values dictionary for Double Q-learning.
+        q2: The secondary Q-values dictionary for Double Q-learning, if applicable.
+    """
+    double = q2 is not None
     state = 'a'
     while state != 'terminal':
         if double:
@@ -136,12 +190,9 @@ def simulate(epoch:int, q:dict, q2=None):
                 q2=q2
                 )
         else:
-            td_update(state, action, new_state, reward, lr, q)
+            q_update(state, action, new_state, reward, lr, q)
 
         state = new_state
-
-
-
 
 
 
@@ -162,25 +213,28 @@ if __name__ == "__main__":
     for i in range(number_of_c_states):
         transitions[f"c{i}"] = ["terminal"]
 
-    normal = []
-    double = []
+    # Track the evolution of Q-values for a specific action over epochs
+    normal = []  # For standard Q-learning
+    double = []  # For Double Q-learning
     
-    q, q1, q2 = {},{},{}
+    # Q-values dictionaries: key=(state,action)
+    q:  Dict[Tuple[str, int], float] = {}
+    q1: Dict[Tuple[str, int], float] = {}
+    q2: Dict[Tuple[str, int], float] = {}
 
     for epoch in range(epochs):
-        # normal
-        simulate(epoch, q)
-        #double Q
-        simulate(epoch, q1, q2)
+        simulate(epoch, q) # normal
+        simulate(epoch, q1, q2) # double
 
-        normal.append(q.get(('a',1),0))
-        d1 = q1.get(('a',1), 0)
-        d2 = q2.get(('a',1), 0)
-        double.append((d1+d2)/2)
+        normal_q_value: float = q.get(('a', 1), 0)
+        double_q_value: float = (q1.get(('a', 1), 0) + q2.get(('a', 1), 0)) / 2
+        normal.append(normal_q_value)
+        double.append(double_q_value)
 
-    plt.plot(normal, label='normal')
-    plt.plot(double, label='double')
+    plt.plot(normal, label='Standard Q-Learning')
+    plt.plot(double, label='Double Q-Learning')
     plt.legend()
-    plt.xlabel('epochs')
-    plt.ylabel("Q value of moving to state B")
+    plt.xlabel('Epochs')
+    plt.ylabel('Value of A -> B')
+    plt.title('Comparison of Q-Learning and Double Q-Learning')
     plt.show()
